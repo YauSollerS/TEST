7ead4c14f2caf0f1117222f227ebcb259a5cce45
Yauheni Baikou <baikou.yauheni@live.com>
2017-06-18 23:37:07 +0300

    commit

---
+++ Test1/SortArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 38, 32, 31, 30, 48, 48, 28, 46, 16, 5, 43, 12, 14, 15, 25, 27 };
+            Array.Sort<int>(array);
+            Array.Reverse(array);
+            foreach (int value in array)
+            {
+                Console.Write(value);
+                Console.Write(' ');
+            }
+            Console.WriteLine();
+        }
+    }
+}
